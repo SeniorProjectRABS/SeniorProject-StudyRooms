d63a6f83040c58819c9c38dd7e41f60b78ea0970
Ezpert <109883612+Ezpert@users.noreply.github.com>
2025-03-25 06:19:00 -0500

    Added Swagger UI

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -53,7 +53,21 @@ INSTALLED_APPS = [
     'django_apscheduler',
     'corsheaders',
     'api',
+    'drf_spectacular'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'TODO List API',
+    'DESCRIPTION': 'API documentation for our app',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'COMPONENT_SPLIT_REQUEST': True
+    # OTHER SETTINGS
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,9 +19,12 @@ Including another URLconf
 
 
 from django.contrib import admin
-from django.urls import path, include  
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
